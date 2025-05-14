6ad179935b2a199b968b1f65563e48dbf0df86f8
Octavio Luna <octavioluna2003@gmail.com>
2025-05-14 02:40:29 +0000

    initial commit

---
+++ ISIS-OBC/isis-obc-first-project/Debug/src/main.d
@@ -0,0 +1,76 @@
+src/main.d: ../src/main.c \
+ C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/freertos/include/freertos/FreeRTOS.h \
+ C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/freertos/include/freertos/projdefs.h \
+ C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/freertos/include/freertos/FreeRTOSConfig.h \
+ C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/at91/include/at91/boards/ISIS_OBC_G20/board.h \
+ C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/at91/include/at91/boards/ISIS_OBC_G20/at91sam9g20/AT91SAM9G20.h \
+ C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/at91/include/at91/utility/trace.h \
+ C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/at91/include/at91/peripherals/dbgu/dbgu.h \
+ C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/at91/include/at91/peripherals/pio/pio.h \
+ C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/at91/include/at91/utility/assert.h \
+ C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/freertos/include/freertos/portable.h \
+ C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/freertos/include/freertos/portable/GCC/ARM9_AT91SAM9G20/portmacro.h \
+ C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/freertos/include/freertos/portable/MemMang/standardMemMang.h \
+ C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/freertos/include/freertos/mpu_wrappers.h \
+ C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/freertos/include/freertos/semphr.h \
+ C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/freertos/include/freertos/queue.h \
+ C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/freertos/include/freertos/task.h \
+ C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/freertos/include/freertos/list.h \
+ C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/hal/include/hal/Timing/WatchDogTimer.h \
+ C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/hal/include/hal/boolean.h \
+ C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/hal/include/hal/Drivers/LED.h \
+ C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/hal/include/hal/Utility/util.h \
+ C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/hal/include/hal/version/version.h \
+ C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/at91/include/at91/peripherals/cp15/cp15.h \
+ C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/at91/include/at91/utility/exithandler.h \
+ C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/at91/include/at91/commons.h
+
+C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/freertos/include/freertos/FreeRTOS.h:
+
+C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/freertos/include/freertos/projdefs.h:
+
+C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/freertos/include/freertos/FreeRTOSConfig.h:
+
+C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/at91/include/at91/boards/ISIS_OBC_G20/board.h:
+
+C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/at91/include/at91/boards/ISIS_OBC_G20/at91sam9g20/AT91SAM9G20.h:
+
+C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/at91/include/at91/utility/trace.h:
+
+C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/at91/include/at91/peripherals/dbgu/dbgu.h:
+
+C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/at91/include/at91/peripherals/pio/pio.h:
+
+C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/at91/include/at91/utility/assert.h:
+
+C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/freertos/include/freertos/portable.h:
+
+C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/freertos/include/freertos/portable/GCC/ARM9_AT91SAM9G20/portmacro.h:
+
+C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/freertos/include/freertos/portable/MemMang/standardMemMang.h:
+
+C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/freertos/include/freertos/mpu_wrappers.h:
+
+C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/freertos/include/freertos/semphr.h:
+
+C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/freertos/include/freertos/queue.h:
+
+C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/freertos/include/freertos/task.h:
+
+C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/freertos/include/freertos/list.h:
+
+C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/hal/include/hal/Timing/WatchDogTimer.h:
+
+C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/hal/include/hal/boolean.h:
+
+C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/hal/include/hal/Drivers/LED.h:
+
+C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/hal/include/hal/Utility/util.h:
+
+C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/hal/include/hal/version/version.h:
+
+C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/at91/include/at91/peripherals/cp15/cp15.h:
+
+C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/at91/include/at91/utility/exithandler.h:
+
+C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/at91/include/at91/commons.h:
---
+++ ISIS-OBC/isis-obc-first-project/Release/src/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/main.c 
+
+OBJS += \
+./src/main.o 
+
+C_DEPS += \
+./src/main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	arm-none-eabi-gcc -mcpu=arm926ej-s -Os -fmessage-length=0 -ffunction-sections -Wall -Wextra -DTRACE_LEVEL=5 -Dat91sam9g20 -Dsdram -D'BASE_REVISION_NUMBER=$(REV)' -D'BASE_REVISION_HASH_SHORT=$(REVHASH_SHORT)' -D'BASE_REVISION_HASH=$(REVHASH)' -I"C:\ISIS\ISIS-OBC\isis-obc-first-project\src" -I"C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/freertos/include" -I"C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/hal/include" -I"C:/ISIS/ISIS-OBC/isis-obc-first-project/..//hal/at91/include" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
